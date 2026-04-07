288375cc78d4d228ef6e07c76435361b46505bc8
jooohn <jooohn12341234@gmail.com>
2026-04-08 00:54:51 +0900

    support allowed dev origins config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const allowedDevOrigins = process.env.AITM_ALLOWED_DEV_ORIGINS
+  ? process.env.AITM_ALLOWED_DEV_ORIGINS.split(",")
+  : [];
+
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins,
 };
 
 export default nextConfig;
